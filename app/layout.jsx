e71b0638dbeda0f18264c63a7da156a0f1de0a29
heinthantzin <heintz414@gmail.com>
2023-11-07 13:13:34 +0630

    add react-query

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,21 +2,24 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar";
 import Footer from "@/components/footer";
+import Providers from "./Providers";
 
 const font = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Hub Store",
-  description: "Clothing Store",
+  title: "E Store",
+  description: "E-Store",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <NavBar />
-        <main>{children}</main>
-        <Footer />
+        <Providers>
+          <NavBar />
+          <main>{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,30 +1,42 @@
+"use client";
 import Link from "next/link";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 import NavLinks from "./nav-links";
 import CartAction from "./cart-action";
 import getCategories from "@/actions/getCategories";
 import SearchForm from "./search-form";
+import { useQuery } from "@tanstack/react-query";
 
-export default async function NavBar() {
-  const categories = await getCategories();
-  // const categories = [
-  //   { id: 1, name: 'Bags', slug: 'bags' },
-  //   { id: 2, name: 'Shoes', slug: 'shoes' },
-  //   { id: 3, name: 'Shirts', slug: 'shirts' },
-  //   { id: 4, name: 'Trousers', slug: 'trousers' },
-  // ];
-  // console.log(categories);
-  // const categories = [];
+export default function NavBar() {
+  // const categories = await getCategories();
+  const {
+    data: categories,
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["categories"],
+    queryFn: getCategories,
+  });
 
+  // if (isPending) return <p>Loading</p>;
+  if (isError) return <p>{error.message}</p>;
+  console.log(categories);
   return (
     <header className="border-b ">
       <div className="container flex items-center justify-between px-4 mx-auto lg:px-8 h-14">
-        <div className="flex items-center">
+        <div className="flex text-center">
           <Link href="/" className="text-3xl font-bold">
             Hub.
           </Link>
-          <NavLinks data={categories} />
+          {isPending ? (
+            <p className="text-center">Loading</p>
+          ) : isError ? (
+            <p>{error.message}</p>
+          ) : (
+            <NavLinks data={categories} />
+          )}
         </div>
 
         <SearchForm />
